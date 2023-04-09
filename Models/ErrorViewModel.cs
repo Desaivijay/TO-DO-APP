61140275a89cd7e0c999fc81c54f782e649c0e7f
Vijay desai <desaivijay918@gmail.com>
2023-04-09 18:19:25 -0400

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TO_DO_APP.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
