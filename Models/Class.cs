34663c957792007ca1deafcddf52cd8bdf21c041
Vijay desai <desaivijay918@gmail.com>
2023-04-12 15:37:47 -0400

    layout created for todos

---
+++ Models/Class.cs
@@ -0,0 +1,10 @@
+﻿namespace TO_DO_APP.Models
+{
+    public class TodoItem
+    {
+        public int Id { get; set; } // Unique identifier for the to-do item
+        public string? Title { get; set; } // Title of the to-do item
+        public bool IsCompleted { get; set; } // Status of the to-do item (completed or not)
+    }
+}
+   
